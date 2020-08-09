399b96989221a5e9aad21da31007b50e8fc6b10c
Anwesh43 <anweshthecool0@gmail.com>
2020-08-10 02:41:23 +0530

    adding EBTRNode class

--- expandtorotbarview/src/main/java/com/anwesh/uiprojects/expandtorotbarview/ExpandBarToRotBarView.kt
+++ expandtorotbarview/src/main/java/com/anwesh/uiprojects/expandtorotbarview/ExpandBarToRotBarView.kt
@@ -122,4 +122,45 @@ class ExpandBarToRotBarView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class EBTRNode(var i : Int, val state : State = State()) {
+
+        private var next : EBTRNode? = null
+        private var prev : EBTRNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = EBTRNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawEBTRNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : EBTRNode {
+            var curr : EBTRNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
